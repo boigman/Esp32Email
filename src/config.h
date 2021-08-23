b64c15040b57625ab48bf8b90cc66eac36cc0a30
boigman <boigman56@gmail.com>
2021-08-23 08:47:04 -0500

    Initial commit

---
+++ src/config.h
@@ -0,0 +1,12 @@
+#define WIFI_SSID "BoigieNetx"
+#define WIFI_PASSWORD "F1r3Engin3Sauc3"
+
+#define SMTP_HOST "smtp.gmail.com"
+#define SMTP_PORT 465
+
+/* The sign in credentials */
+#define AUTHOR_EMAIL "boiglar.esp32@gmail.com"
+#define AUTHOR_PASSWORD "Kevlar1986!"
+
+/* Recipient's email*/
+#define RECIPIENT_EMAIL "boigman56@gmail.com"
